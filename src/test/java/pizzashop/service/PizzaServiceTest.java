e19c88848baf298073432c0a73a769d9c0c89423
Gheorghe Alex <freezul123@yahoo.com>
2022-03-20 20:16:45 +0200

    Update PizzaServiceTest.java
    ECP Test Cases

--- src/test/java/pizzashop/service/PizzaServiceTest.java
+++ src/test/java/pizzashop/service/PizzaServiceTest.java
@@ -3,5 +3,71 @@ package pizzashop.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PizzaServiceTest {
+    @DisplayName("Test tables valid")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2})
+    void testTablesV(int value) throws Exception {
+        //Arrange
+        double amount = 20d;
+        PaymentType paymentType = PaymentType.Card;
+        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
 
+        //Act
+        boolean result = pizzaService.addPayment(value, paymentType, amount);
+
+        //Assert
+        assertEquals(true, result);
+    }
+
+    @RepeatedTest(2)
+    void testAmmountV() throws Exception {
+        //Arrange
+        int t = 3;
+        PaymentType paymentType = PaymentType.Card;
+        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
+
+        //Act
+        boolean result = pizzaService.addPayment(t, paymentType, 20d);
+
+        //Assert
+        assertEquals(true, result);
+    }
+
+    @Timeout(1000)
+    void testPaymentTypeV() throws Exception {
+        //Arrange
+        int t = 3;
+        PaymentType paymentType = PaymentType.Cash;
+        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
+
+        //Act
+        boolean result = pizzaService.addPayment(t, paymentType, 20d);
+
+        //Assert
+        assertEquals(true, result);
+    }
+
+    @DisplayName("Test tables invalid")
+    @ParameterizedTest
+    @ValueSource(ints = {-100, 90})
+    void testTablesN(int value) throws Exception {
+        //Arrange
+        double amount = 20d;
+        PaymentType paymentType = PaymentType.Card;
+        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
+
+        //Act
+        assertThrows(Exception.class, () -> {pizzaService.addPayment(value, paymentType, amount);});
+    }
+
+    @RepeatedTest(2)
+    void testAmmountN() throws Exception {
+        //Arrange
+        int t = 3;
+        PaymentType paymentType = PaymentType.Card;
+        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
+
+        //Act
+        assertThrows(Exception.class, () -> {pizzaService.addPayment(t, paymentType, -20d);});
+    }
 }
